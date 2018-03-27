3606794985607de3f962a1020ce262496e50f7dc
Lorenzo Corbella <ing.lorenzo.corbella@gmail.com>
2018-03-28 01:22:29 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material';
+
+import { AddDialogueComponent } from './app/components/add-dialogue/add-dialogue.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  projects: any[] = [
+    { id: '1', name: 'Structures', description: 'bla bla...', ambito: 'Personale' },
+    { id: '2', name: 'Passwords', description: 'bla bla...', ambito: 'Personale' }
+  ];
+
+  constructor(public dialog: MatDialog) {
+
+  }
+
+  openAddDialog() {
+    const dialogRef = this.dialog.open(AddDialogueComponent, {
+      height: '450px',
+      width: '600px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  showInfo(id) {
+    console.log(id);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
+// MODULES
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from './material.module';
 
-
+// COMPONENTS
 import { AppComponent } from './app.component';
+import { AddDialogueComponent } from './app/components/add-dialogue/add-dialogue.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, AddDialogueComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    BrowserAnimationsModule
+  ],
+  entryComponents: [
+    AddDialogueComponent
+  ],
+  exports: [
   ],
   providers: [],
   bootstrap: [AppComponent]
